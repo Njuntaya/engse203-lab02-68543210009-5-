917951b72b5a6d0f9ef0e36c77217f35c7e88443
Njuntaya <ruizfeez99@gmail.com>
2026-07-12 21:58:25 +0700

    fix vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 
-// TODO: เปลี่ยนให้ตรงชื่อ repository ของตนเอง เช่น engse203-lab02-67123456789
-const repositoryName = "engse203-lab02-<68543210009-5>";
+const repositoryName = "engse203-lab02-68543210009-5";
 
 export default defineConfig({
   base: `/${repositoryName}/`,
